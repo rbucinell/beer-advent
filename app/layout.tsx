4777df66cb633e51ea34c2d663018d6a06707b74
Ryan Bucinell <rbucinell.dev@gmail.com>
2023-10-22 23:38:53 -0400

    added basic nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './globals.css'
+import Nav from '@/components/Nav';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Nav />
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
---
+++ components/Nav.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Nav() {
+    return (
+        <nav>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                    {/*<IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                        <MenuIcon />
+                    </IconButton> */}
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <Link href="/">Home </Link>
+                    </Typography>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <Link href="/history">History </Link>
+                    </Typography>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <Link href="/addbeer">Add Beer </Link>
+                    </Typography>
+                    {/* <Button color="inherit">Login</Button> */}
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </nav>
+    );
+}
