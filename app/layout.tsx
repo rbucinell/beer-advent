0191643a4094b7e15dd7beaf76a07080a0c9ad6d
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-10-15 09:15:18 -0400

    added clerk middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import '@fontsource/roboto/700.css';
 import { Container, CssBaseline } from '@mui/material';
 import './globals.css'
 import Nav from '@/components/Nav';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,15 +20,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      
-        <body className={`${inter.className} bg-green-200 repeating-beer-bg`}>
-          <CssBaseline /> 
-          <Nav />      
-          <Container sx={{ p:2, minHeight: '70vh'}}>          
-            {children}
-          </Container>
-        </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        
+          <body className={`${inter.className} bg-green-200 repeating-beer-bg`}>
+            <CssBaseline /> 
+            <Nav />      
+
+            <Container sx={{ p:2, minHeight: '70vh'}}>          
+              {children}
+            </Container>
+          </body>
+      </html>
+    </ClerkProvider>
   )
 }
