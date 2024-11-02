51f1a88444acb24628e52307b8e5aa39a4968331
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-11-02 01:08:37 -0400

    adding User database object and CRUD api

---
+++ app/api/user/[id]/route.ts
@@ -0,0 +1,55 @@
+import connectDB from '@/app/lib/mongodb';
+import User from '@/app/models/user';   
+import mongoose from 'mongoose';
+import { NextRequest, NextResponse } from "next/server";
+import { use } from 'react';
+
+export async function GET( req:NextRequest ) {
+    try {
+        await connectDB();
+        const id = req.nextUrl.pathname.split('/').pop();
+        const user = await User.findById( id );
+        if( !user ) return NextResponse.json( { msg: ["User not found"] }, { status: 404 } );
+        return NextResponse.json( user );
+    }catch( error ) {
+        console.log( error );
+        return NextResponse.json({msg: ["Unable to retrieve User. " + error] }, { status: 500 });
+    }
+}
+
+export async function DELETE( req:NextRequest ) {
+    try {
+        await connectDB();
+        const id = req.nextUrl.pathname.split('/').pop();
+        const user = await User.findById( id );
+        if( !user ) return NextResponse.json( { msg: ["User not found"] }, { status: 404 } );
+        if( user.deleted ) return NextResponse.json( { msg: ["User already deleted"] }, { status: 400 } );
+        const deleteResponse = await User.deleteOne( user );
+        if( !deleteResponse.deletedCount ) return NextResponse.json( { msg: ["Unable to delete User"] }, { status: 500 } );
+        return new NextResponse( null, { status: 204 } );
+    }catch( error ) {
+        console.log( error );
+        return NextResponse.json({msg: ["Unable to delete User. " + error] }, { status: 500 });
+    }
+}
+
+export async function PUT( req:NextRequest ) {
+    try {
+        await connectDB();
+        const id = req.nextUrl.pathname.split('/').pop();
+        const user = await User.findById( id );
+        if( !user ) return NextResponse.json( { msg: ["User not found"] }, { status: 404 } );
+        const json = await req.json();
+
+        if( json.firstName) user.firstName = json.firstName;
+        if( json.lastName) user.lastName = json.lastName;
+        if( json.email) user.email = json.email;
+        if( json.imageUrl) user.imageUrl = json.imageUrl;
+        await user.save();       
+        return new NextResponse( user, { status: 204 } );
+        
+    }catch( error ) {
+        console.log( error );
+        return NextResponse.json({msg: ["Unable to user User. " + error] }, { status: 500 });
+    }
+}
---
+++ app/models/user.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+export interface IUser extends Document {
+    firstName: string;
+    lastName: string;
+    email: string;
+    imageUrl: string;
+}
+
+export const userSchema = new Schema<IUser>(
+    {
+        firstName:{
+            type: String,
+            required: [true, "First name is required"],
+        },
+        lastName: {
+            type: String,
+            required: [true, "Last name is required"],
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true
+        },
+        imageUrl: String
+    },
+    {
+        collection: 'users',
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+);
+
+const User = mongoose.models.User || mongoose.model<IUser>('User', userSchema);
+export default User;
