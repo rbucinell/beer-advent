3441c7fb0aab820553898a94c918eaf265765d83
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-11-05 02:25:36 -0500

    imporvement to intial load

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react";
 import {
-  Avatar,
   Button,
   ButtonGroup,
   Paper,
@@ -19,6 +18,12 @@ import { IUser } from "@/app/models/user";
 import { DateRange, SortByAlpha } from "@mui/icons-material";
 import PendingItem from "@/components/PendingItem";
 
+type IData = {
+  event: IEvent;
+  participants: IParticipant[];
+  users: IUser[];
+}
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -28,24 +33,23 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function Home() {
-  const [advent, setAdvent] = useState<IEvent>();
-  const [participants, setParticipants] = useState<IParticipant[]>([]);
-  const [users, setUsers] = useState<IUser[]>([]);
+
+  const [data, setData] = useState({} as IData);
 
   useEffect(() => {
     (async () => {
-      let eventResp = await Get<IEvent>(`api/event?year=${new Date().getFullYear()}`);
-      console.log(eventResp);
-      setAdvent(eventResp);
-      let participantResp = await Get<IParticipant[]>(`api/participant?event=${eventResp._id}`);
-      let participatingUsers: IUser[] = [];
-      for( const participant of participantResp ) {
-        let userResp = await Get<IUser>(`api/user/${participant.user}`);
-        participatingUsers.push(userResp);
+
+      const event = await Get<IEvent>(`api/event?year=${new Date().getFullYear()}`);
+      if( !event._id ) return;
+      const participants = await Get<IParticipant[]>(`api/participant?event=${event._id}`);
+      let users:IUser[] = [];
+
+      for( const participant of participants ) {
+        const user = await Get<IUser>(`api/user/${participant.user}`);
+        users.push(user);
       }
-      setUsers(participatingUsers);
-      console.log(participantResp);
-      setParticipants(participantResp);
+      setData({event, participants, users});
+
     })();
   }, []);
 
@@ -56,24 +60,6 @@ export default function Home() {
   function sortParticipantsDate(event: any) {
     console.log("sortParticipantsDate");
   }
-
-  const handleClick = async (
-    e: React.MouseEvent<HTMLButtonElement>
-  ): Promise<void> => {
-    e.preventDefault();
-    console.log("handleClick:");
-    const res = await fetch("/api/participant/register", {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        //"event": advent,
-        //"user": user
-      }),
-    });
-    const { msg, success } = await res.json();
-    console.log(msg);
-    console.log(success);
-  };
 
   return (
     <div className="p-2 max-w-3xl mx-auto">
@@ -86,46 +72,19 @@ export default function Home() {
           <DateRange />
         </Button>
         </ButtonGroup>
-        <Typography variant="h6">{advent?.name}🎄</Typography>
+        <Typography variant="h6">{data?.event?.name}🎄</Typography>
       </Stack>
       <Stack spacing={0.5}>
         {Array.from({ length: 12 }, (_, index) =>
-          participants && participants[index] ? (
-            <ParticipantItem key={participants[index]._id.toString()} 
-              participant={participants[index]} 
-              user={ users.find(u => u._id === participants[index].user) as IUser}/>
+          data?.participants && data?.participants[index] ? (
+            <ParticipantItem key={data?.participants[index]._id.toString()} 
+              participant={data?.participants[index]} 
+              user={ data?.users.find(u => u._id === data?.participants[index].user) as IUser}/>
           ) : (
             <PendingItem key={index}/>
           )
         )}
       </Stack>
-      {/* {participants.length < 12 && (
-        <Button
-          sx={{ mt: 2 }}
-          color="primary"
-          variant="contained"
-          onClick={handleClick}
-        >
-          {`Register for Event`}
-        </Button>
-      )} */}
     </div>
   );
-
-  // return (
-  //   <div className='p-2 max-w-3xl mx-auto'>
-  //     {/* <Typography variant="h4" sx={{ m: 1 }}>
-  //       { advent ? advent.name : "Beer Advent"}
-  //     </Typography> */}
-  //     <Stack spacing={1}>
-  //       { participants ?
-  //         participants.map( (participant) =>
-  //         (
-  //           <ParticipantItem key={participant._id.toString()} {...participant}/>
-  //         ) )
-  //         : (<Item> Loading Participants </Item>)
-  //       }
-  //   </Stack>
-  //   </div>
-  // );
 }
