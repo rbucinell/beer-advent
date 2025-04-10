0fb80e41f60df0401c5a36a0f305ae91f530cdb6
Ryan Bucinell <rbucinell.dev@gmail.com>
2025-04-10 14:07:03 -0400

    updated api for events CRUD

---
+++ app/api/events/[year]/route.ts
@@ -0,0 +1,57 @@
+import connectDB from '@/app/lib/mongodb';
+import Event from '@/app/models/event';
+import { Error } from 'mongoose';
+import { NextRequest, NextResponse } from "next/server";
+
+/**
+ * Get a particular event
+ * @param req 
+ * @returns 
+ */
+export async function GET( req:NextRequest ) {
+    try{
+        await connectDB();
+        const year = req.nextUrl.pathname.split('/').pop();
+        const event = await Event.findOne<Event>({ year });
+        if( !event ){
+            return NextResponse.json( { msg: `Event for ${year} not found` }, { status: 404 } );
+        }
+        return NextResponse.json<Event>(event);
+    }catch( error ) {
+        console.log( error );
+        return NextResponse.json({msg: ["Unable to retrieve event from Id"], error }, { status: 500 });
+    }
+}
+
+// # To update particular event.
+export async function PUT( req:NextRequest ) {
+    try{
+        await connectDB();
+        const year = req.nextUrl.pathname.split('/').pop();
+        const query = {year};
+        const requestBody = await req.json();
+        console.log( 'query:', query, 'requestBody:', requestBody)
+        const updateResponse = await Event.findOneAndUpdate(query, requestBody, { includeResultMetadata: true} );
+        console.log( 'Event [PUT], Event.findOneAndUpdate: ', updateResponse );
+        return updateResponse.ok === 1 
+            ? new NextResponse(null, { status: 204})
+            : new NextResponse(updateResponse.value, { status: 400} );
+    } catch( error) {
+        console.log( error );
+        return NextResponse.json({msg:"Error updating event", error: (error as Error).message }, { status: 500 });
+    }
+}
+
+// # To delete particular user
+export async function DELETE( req:NextRequest ) {
+    try{
+        await connectDB();
+        const year = req.nextUrl.pathname.split('/').pop();
+        const query = {year};
+        const updateResponse = await Event.findOneAndDelete(query);
+        return NextResponse.json({deleted: updateResponse}, { status: 200 });
+    } catch( error) {
+        console.log( error );
+        return NextResponse.json({msg:"Error updating event", error: (error as Error).message }, { status: 500 });
+    }
+}
